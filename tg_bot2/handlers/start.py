a86ba4d47bfd82e5e1cb17d1f7e5aff3f4ce3b32
0xshodan <hngdean@gmail.com>
2023-05-25 15:14:00 +0300

    create user model

--- tg_bot2/handlers/start.py
+++ tg_bot2/handlers/start.py
@@ -8,7 +8,15 @@ from tg_bot2.keyboards.default import start_keyboard
 
 
 async def welcome_handler(message: types.Message, state: FSMContext):
-    _us = User
+    User.get_or_create(
+        id=message.from_user.id,
+        defaults={
+            "id":message.from_user.id,
+            "telegram_first_name":message.from_user.first_name,
+            "telegram_last_name":message.from_user.last_name,
+            "telegram_username":message.from_user.username,
+            }
+            )
     await state.finish()
     photo = types.InputFile(config.START_IMAGE_PATH)
     await message.answer_photo(photo, config.START_TEXT,reply_markup=start_keyboard())
