ea518db947cae73114d2a2cc1503a393a3a77161
0xshodan <hngdean@gmail.com>
2023-05-25 16:33:02 +0300

    update general info

--- tg_bot2/handlers/collect_general_info.py
+++ tg_bot2/handlers/collect_general_info.py
@@ -2,7 +2,7 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from tg_bot2.data.models import User
-from tg_bot2.states import GeneralInfo
+from tg_bot2 import states
 from typing import Optional
 
 
@@ -10,19 +10,19 @@ async def entrypoint(message: types.Message, state: FSMContext, external_type: s
     user = await User.get(id=message.from_user.id)
     await state.set_data({"user": user, "type":external_type})
     if user.name is None:
-        await GeneralInfo.name.set()
+        await states.GeneralInfo.name.set()
         await message.answer("Для начала введите своё имя:")
     elif user.email is None:
-        await GeneralInfo.email.set()
+        await states.GeneralInfo.email.set()
         await message.answer("Теперь укажите почту")
     elif user.phone is None:
-        await GeneralInfo.phone.set()
+        await states.GeneralInfo.phone.set()
         await message.answer("Укажите ваш телефон")
     elif user.city is None:
-        await GeneralInfo.city.set()
+        await states.GeneralInfo.city.set()
         await message.answer("Ваш город:")
     elif user.communication is None:
-        await GeneralInfo.communication.set()
+        await states.GeneralInfo.communication.set()
         await message.answer("Предпочитаемый способ связи (телеграм/телефон/почта):")
     else:
         return user
@@ -31,34 +31,45 @@ async def collect_name(message: types.Message, state: FSMContext):
     data = await state.get_data()
     user = data["user"]
     user.name = message.text
-    await GeneralInfo.email.set()
+    await states.GeneralInfo.email.set()
+    await message.answer("Теперь укажите почту")
 
 async def collect_email(message: types.Message, state: FSMContext):
     data = await state.get_data()
     user = data["user"]
     user.email = message.text
-    await GeneralInfo.phone.set()
+    await states.GeneralInfo.phone.set()
+    await message.answer("Укажите ваш телефон")
 
 async def collect_phone(message: types.Message, state: FSMContext):
     data = await state.get_data()
     user = data["user"]
     user.phone = message.text
-    await GeneralInfo.city.set()
+    await states.GeneralInfo.city.set()
+    await message.answer("Ваш город:")
 
 async def collect_city(message: types.Message, state: FSMContext):
     data = await state.get_data()
     user = data["user"]
     user.city = message.text
-    await GeneralInfo.communication.set()
+    await states.GeneralInfo.communication.set()
+    await message.answer("Предпочитаемый способ связи (телеграм/телефон/почта):")
 
 async def collect_communication(message: types.Message, state: FSMContext):
     data = await state.get_data()
     user = data["user"]
     user.communication = message.text
+    if data["type"] == "consultation":
+        await states.Consultation.question.set()
+        await message.answer("Вопрос по которому необходима консультация: ")
+    elif data["type"] == "training":
+        await states.Training.description.set()
+    elif data["type"] == "analysis":
+        await states.Analysis.project_url.set()
 
 def register_general_info_handlers(dp: Dispatcher):
-    dp.register_message_handler(collect_name, state=GeneralInfo.name)
-    dp.register_message_handler(collect_email, state=GeneralInfo.email)
-    dp.register_message_handler(collect_phone, state=GeneralInfo.phone)
-    dp.register_message_handler(collect_city, state=GeneralInfo.city)
-    dp.register_message_handler(collect_communication, state=GeneralInfo.communication)
+    dp.register_message_handler(collect_name, state=states.GeneralInfo.name)
+    dp.register_message_handler(collect_email, state=states.GeneralInfo.email)
+    dp.register_message_handler(collect_phone, state=states.GeneralInfo.phone)
+    dp.register_message_handler(collect_city, state=states.GeneralInfo.city)
+    dp.register_message_handler(collect_communication, state=states.GeneralInfo.communication)
