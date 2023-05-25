44dbe6d70d2d9b31c791a7f14b4445d44d7828ef
0xshodan <hngdean@gmail.com>
2023-05-25 14:14:03 +0300

    start handler v1

--- tg_bot2/data/config.py
+++ tg_bot2/data/config.py
@@ -1,10 +1,12 @@
 from environs import Env
-
+from os.path import isfile
 # Теперь используем вместо библиотеки python-dotenv библиотеку environs
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")
-ADMINS = env.list("ADMINS")  # Список админов
-START_TEXT = env.str("START_TEXT")  # Приветственный текст
-START_IMAGE = env.list("START_IMAGE")  # Тут у нас будет список из админов
+BOT_TOKEN: str = env.str("BOT_TOKEN")
+ADMINS: list[str] = env.list("ADMINS")  # Список админов
+START_TEXT: str = env.str("START_TEXT")  # Приветственный текст
+START_IMAGE_PATH: str = env.str("START_IMAGE_PATH")  # Тут у нас будет список из админов
+if not isfile(START_IMAGE_PATH):
+    raise FileNotFoundError("Ошибка: неверно указан путь для приветсвенного изображения")
--- tg_bot2/handlers/start.py
+++ tg_bot2/handlers/start.py
@@ -2,9 +2,12 @@
 from aiogram.dispatcher.filters import CommandStart
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
+from tg_bot2.data import config
 
 async def welcome_handler(message: types.Message, state: FSMContext):
-    pass
+    await state.finish()
+    photo = types.InputFile(config.START_IMAGE_PATH)
+    await message.answer_photo(photo, config.START_TEXT)
 
 
 def register_start_handler(dp: Dispatcher):
