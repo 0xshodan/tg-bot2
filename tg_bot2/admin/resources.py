6062fae4033e58a85e82da1185e49233e56e46b5
0xshodan <hngdean@gmail.com>
2023-05-26 11:35:17 +0300

    finish

--- tg_bot2/admin/resources.py
+++ tg_bot2/admin/resources.py
@@ -2,7 +2,7 @@ from fastapi_admin.app import app
 from fastapi_admin.resources import Link
 from fastapi_admin.resources import Field, Model
 from tg_bot2.data.models import User, Analyze, Question
-from fastapi_admin.widgets import filters, inputs
+from fastapi_admin.widgets import filters, inputs, displays
 from fastapi_admin.file_upload import FileUpload
 import os
 
@@ -29,7 +29,7 @@ class UserResource(Model):
     ]
     fields = [
         "id",
-        "telegram_username",
+        Field(name="telegram_username", label="telegram"),
         "name",
         "email",
         "phone",
@@ -44,14 +44,14 @@ class UserResource(Model):
 class AnalysisResource(Model):
     label = "Заявки на анализ"
     model = Analyze
-    icon = "fas fa-user"
+    icon = "fas fa-chart-bar"
     page_title = "Заявки на анализ"
     filters = [
     ]
     fields = [
         "id",
-        "user",
-        Field("project_url", "Ссылка на проект"),
+        Field("user", "Пользователь"),
+        Field("project_url", "Ссылка на проект", display=displays.Url()),
         "app",
         "other_info"
     ]
@@ -60,12 +60,9 @@ class AnalysisResource(Model):
 class QuestionResource(Model):
     label = "Вопросы"
     model = Question
-    icon = "fas fa-user"
-    page_title = "Пользователи"
+    icon = "fas fa-question"
+    page_title = "Вопросы"
     filters = [
-        filters.Search(
-            name="telegram_username", label="Юзернейм", search_mode="contains", placeholder="Поиск по юзернейму"
-        ),
     ]
     fields = [
         "id",
